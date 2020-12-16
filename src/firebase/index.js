d1f65f596c89e8cb8b9eb3342d244b4daac28df1
EdiAfremov <EdiA@radware.com>
2020-12-16 22:36:17 +0200

    added log

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,8 @@ import "firebase/auth";
 
 export const { auth } = app;
 
+console.log(process.env.REACT_APP_API_KEY);
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
